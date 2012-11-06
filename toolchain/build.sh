e29f39f6f195bb47e18e4ace27207c154ac2d326
Christopher Nelson <nadiasvertex@gmail.com>
2012-11-06 07:47:15 -0500

    Updated build.sh to build libjit.

--- toolchain/build.sh
+++ toolchain/build.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
 BINDIR=$PWD/tools
 mkdir -p $BINDIR
+
 # Build protobuf
-cd protobuf-2.4.1
-./configure --prefix=$BINDIR
-make && make install
-cd ..
+#cd protobuf-2.4.1
+#./configure --prefix=$BINDIR
+#make && make install
+#cd ..
 
 # Build 0MQ
-cd zeromq-3.2.1
-./configure --prefix=$BINDIR --with-pgm --disable-shared --enable-static
-make && make install
+#cd zeromq-3.2.1
+#./configure --prefix=$BINDIR --with-pgm --disable-shared --enable-static
+#make && make install
+#cd ..
+
+# Build libjit
+cd libjit-0.1.2
+./configure --prefix=$BINDIR --disable-shared --enable-static
+cd jit && make && make install && cd ..
+cd jitdynamic && make && make install && cd ..
+cd jitplus && make && make install && cd ..
 cd ..
 
 cp -f zmqcpp/zmq.hpp $BINDIR/include
