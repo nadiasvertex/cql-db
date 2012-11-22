495e7c964d037ad9469c9fbe960f549a0295fb06
Christopher Nelson <nadiasvertex@gmail.com>
2012-11-22 13:50:48 -0500

    Added primoridal address object.

---
+++ src/lib/edge/cpp/address.h
@@ -0,0 +1,20 @@
+#ifndef __LATTICE_EDGE_ADDRESS_H__
+#define __LATTICE_EDGE_ADDRESS_H__
+
+#include <edge/proto/address.pb.h>
+
+namespace lattice {
+namespace edge {
+
+class address
+{
+
+public:
+   address() {}
+
+};
+
+} // end namespace edge
+} // end namespace lattice
+
+#endif // __LATTICE_EDGE_ROUTER_H__
